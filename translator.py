d6333209df104bdcd94ba32082da26b7a843eb3f
stranger <alirezaz313313@gmail.com>
2024-08-13 11:56:35 +0330

    .

--- translator.py
+++ translator.py
@@ -1,13 +1,20 @@
 import sys
 from deep_translator import GoogleTranslator
 
-sys.stdout.reconfigure(encoding = 'utf-8')
-
 def translate(text, language):
     translator = GoogleTranslator(source = 'auto', target = language)
-    translator = translator.translate(text)
-    
-    return translator
+    translated = translator.translate(text)
+    return translated 
 
+def write_words_to_file(translated_text, filename):
+    words = translated_text.split()
+    with open(filename, 'w', encoding= 'utf-8') as file: 
+        for word in words:
+            file.write(word + '\n')
+            
 txt_input = 'hello to you'
 translated = translate(txt_input, 'fa')
+
+
+filename = 'translated_words.txt'
+write_words_to_file(translated, filename)
